ddcb9ce36eaac20cc1201a374c74e2ef670cb3dd
Suyeol Yun <syyun@mit.edu>
2023-02-06 15:37:45 -0500

    add annul parser

--- utils.py
+++ utils.py
@@ -28,6 +28,48 @@ def get_senators(n_th_congress):
         pass
     return df
 
+def parse_annual(url):
+    import selenium
+    from selenium import webdriver
+    from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.common.by import By
+
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    # from lobbyview.db import PostgresqlManager
+
+    driver = webdriver.Chrome(
+        ChromeDriverManager().install(), chrome_options=chrome_options
+    )
+
+    # get base url and click on agreement
+    base_url = "https://efdsearch.senate.gov/search/"
+    driver.get(base_url)
+    driver.find_element(By.ID, "agree_statement").click() 
+
+    # go to actual target url which contains the annual report
+    driver.get(url)
+    html = driver.page_source
+
+    bs = BeautifulSoup(html, "html.parser")
+    tables = bs.find_all("table")
+    tbody = tables[3].find("tbody")
+    trs = tbody.find_all("tr")
+    for tr in trs:
+        tds = tr.find_all("td")
+        if len(tds) > 1:
+            print(tds[0].text, tds[1].text)
+    pass
+
+
+
+    pass
+
+
+
+
 if __name__ == "__main__":
-    df = get_senators(n_th_congress=118)
+    # df = get_senators(n_th_congress=118)
+    df = parse_annual(url="https://efdsearch.senate.gov//search/view/annual/2f72a35a-adad-4555-8341-01eacbd507d3/")
     pass
