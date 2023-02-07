ce05c495afa988b42ef5ab1ad647a25cbe67fd96
Suyeol Yun <syyun@mit.edu>
2023-02-07 12:10:27 -0500

    Auto stash before cherry pick of "ignore"

---
+++ tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [IF NOT EXISTS] senator (
+   first_name text,
+   last_name text,
+   url text
+);
+
+
+CREATE TABLE [IF NOT EXISTS] senate_annual (
+   first_name text,
+   last_name text,
+   office text,
+   report_type text,
+   report_type_url text,
+   date_received datetime
+);
--- utils.py
+++ utils.py
@@ -6,8 +6,11 @@ import pandas as pd
 def get_senators(n_th_congress):
     wiki_url = f"https://en.wikipedia.org/wiki/List_of_United_States_senators_in_the_{n_th_congress}th_Congress"
     df = pd.DataFrame({
+            'congress': [],
             'first_name': [],
             'last_name': [],
+            'party': [],
+            'url': [],
            })
 
     r = requests.get(wiki_url)
@@ -19,16 +22,17 @@ def get_senators(n_th_congress):
     table = tables[1]
     trs = table.find_all('tr')
     for tr in trs:
-        td = tr.find('td')
-        if td is not None:
-            name = td.text
+        tds = tr.find_all('td')
+        if len(tds) > 0:
+            name = tds[0].text
             first_name, last_name = name.split(' ')[0].replace('\n', '').split('[')[0], name.split(' ')[-1].replace('\n', '').split('[')[0]
-            df.loc[len(df.index)] = [first_name, last_name]
-        # name = td.data_sort_value
-        pass
+            url = tds[0].find('a')['href']
+            party = tds[1].text
+            df.loc[len(df.index)] = [n_th_congress, first_name, last_name, party, 'https://en.wikipedia.org/' + url]   
     return df
 
 
 if __name__ == "__main__":
     df = get_senators(n_th_congress=118)
+    # df = parse_annual(url="https://efdsearch.senate.gov//search/view/annual/2f72a35a-adad-4555-8341-01eacbd507d3/")
     pass
