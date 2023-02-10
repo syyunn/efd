62575cfbd2433cd36802e3afaacdd86778699331
Suyeol Yun <syyun@mit.edu>
2023-02-10 09:08:07 -0500

    add amount_max calculator

--- cash-out.py
+++ cash-out.py
@@ -4,7 +4,8 @@ result = pd.DataFrame({
         'first_name': [],
         'last_name': [],
         'ticker': [],
-        'cash': []
+        'cash(amount_min)': [],
+        'cash(amount_max)': []
         })
 
 congress = 117
@@ -24,7 +25,7 @@ for row in df: # name and ticker pairs
     backslash_char = '\''
     trnscs = pm.execute_sql(fetchall=True, sql=
                 f"""
-select distinct first_name, last_name, p.ticker, trans_type, trans_date, amount_min, vwap from senate_annual_4b sb 
+select distinct first_name, last_name, p.ticker, trans_type, trans_date, amount_min, vwap, amount_max from senate_annual_4b sb 
 inner join senate_annual sa on sa.report_type_url  = sb.report_url 
 inner join price p on (p.ticker=sb.ticker and p.date=sb.trans_date)
 where first_name = {backslash_char}{row[0]}{backslash_char} and last_name = {backslash_char}{row[1]}{backslash_char} and sb.ticker = {backslash_char}{row[2]}{backslash_char}
@@ -33,57 +34,44 @@ order by trans_date asc
                 )
     # check only the cases which starts as purchase and ends as sell
     if len(trnscs) >1: # for the case of ticker is None
-        # if "Purchase" in trnscs[0][3] and "Sale (Full)" in trnscs[-1][3]:
-        # if "Purchase" in trnscs[0][3] and "Sale (Full)" in trnscs[-1][3]:
-        #     print(row[0], row[1], row[2])
-        cash = 0
-        purchase_units = []
+        cash_amin = 0
+        cash_amax = 0
+
+        purchase_units_amin = []
+        purchase_units_amax = []
 
         for trnsc in trnscs: # all transactions of a name and ticker pair
             ticker = trnsc[2]
             ps = trnsc[3] # purchase or sale
             date = trnsc[4]
-            amount = trnsc[5]
+            amount_min = trnsc[5]
             vwap = trnsc[6]
+            amount_max = trnsc[7]
+            
+            def _cash_out(purchase_units, cash, ps, amount, vwap):
+                current_holdings = sum(purchase_units)
 
-            current_holdings = sum(purchase_units)
+                if "Purchase" in ps:
+                    units = amount / vwap # this is the minimum amount of units purchased
+                    purchase_units.append(units)
+                    cash -= amount
+                elif "Sale" in ps:
+                    units = amount / vwap # minimum units estimated as being sold.
+                    if units > current_holdings: # this means we're missing previous data about purchasement
+                        purchase_units.append(-current_holdings) # sell all of the holdings
+                        cash_out = current_holdings * vwap
+                        cash += cash_out                    
+                    else:
+                        purchase_units.append(-units)
+                        cash_out = units * vwap
+                        cash += cash_out
+                return purchase_units, cash
 
-            if "Purchase" in ps:
-                units = amount / vwap # this is the minimum amount of units purchased
-                purchase_units.append(units)
-                cash -= amount
-            elif "Sale" in ps:
-                units = amount / vwap # minimum units estimated as being sold.
-                if units > current_holdings: # this means we're missing previous data about purchasement
-                    purchase_units.append(-current_holdings) # sell all of the holdings
-                    cash_out = current_holdings * vwap
-                    cash += cash_out
-                else:
-                    purchase_units.append(-units)
-                    cash_out = units * vwap
-                    cash += cash_out
-
-            # elif "Sale (Full)" in ps:
-            #     units = amount / bars[0].vwap # minimum units estimated as being sold.
-            #     if units > current_holdings: # this means we're missing previous data about purchasement
-            #         print("break! insufficient holdings to sell all", row[0], row[1], row[2])
-            #         break
-            #     else:
-            #         purchase_units.append(-current_holdings) # sell all of the holdings
-            #         cash_out = current_holdings * bars[0].vwap
-            #         cash += cash_out      
-            # elif "Sale (Partial)" in ps:
-            #     units = amount / bars[0].vwap # minimum amount estimated as being sold.
-            #     if units > current_holdings: # this means we're missing previous data because one can't sell more than what they have
-            #         print("break! insufficient holdings to sell", row[0], row[1], row[2])
-            #         break
-            #     else:
-            #         purchase_units.append(-units)
-            #         cash_out = units * bars[0].vwap
-            #         cash += cash_out
+            purchase_units_amin, cash_amin = _cash_out(purchase_units_amin, cash_amin, ps, amount_min, vwap)
+            purchase_units_amax, cash_amax = _cash_out(purchase_units_amax, cash_amax, ps, amount_max, vwap)
             pass
-        result.loc[len(result.index)] = [row[0], row[1], row[2], cash]  
-        print(row[0], row[1], row[2], cash)
+        result.loc[len(result.index)] = [row[0], row[1], row[2], cash_amin, cash_amax]  
+        print(row[0], row[1], row[2], cash_amin, cash_amax)
 
 
     pass
