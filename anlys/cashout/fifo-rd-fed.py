997e5239004c5c6859046c2be37cdc4966f3e8d0
Suyeol Yun <syyun@mit.edu>
2023-02-14 17:16:07 -0500

    add fed-rates adju

--- anlys/cashout/fifo-rd-fed.py
+++ anlys/cashout/fifo-rd-fed.py
@@ -4,8 +4,6 @@ import pandas as pd
 import random
 import numpy as np
 
-# read fed rates
-fed_rates = pd.read_csv("external-data/FEDFUNDS.csv")
 
 result = pd.DataFrame({
         'first_name': [],
@@ -38,6 +36,9 @@ df = pm.execute_sql(fetchall=True, sql=
 # row = ('Ron L', 'Wyden', 'AMAT')
 # row = ('Sheldon', 'Whitehouse', 'QQQ')
 
+# read fed rates
+fed_rates = pd.read_csv("external-data/FEDFUNDS.csv")
+
 for row in df: # name and ticker pairs
     print(row)
     backslash_char = '\''
@@ -51,7 +52,7 @@ for row in df: # name and ticker pairs
                 select distinct first_name, last_name, p.ticker, trans_type, trans_date, amount_min, vwap, amount_max from union4ab u
                     inner join senate_annual sa on sa.report_type_url  = u.report_url 
                     inner join price p on (p.ticker=u.ticker and p.date=u.trans_date)
-                where first_name = {backslash_char}{row[0]}{backslash_char} and last_name = {backslash_char}{row[1]}{backslash_char} and u.ticker = {backslash_char}{row[2]}{backslash_char} and vwap is not null
+                where first_name = {backslash_char}{row[0]}{backslash_char} and last_name = {backslash_char}{row[1]}{backslash_char} and u.ticker = {backslash_char}{row[2]}{backslash_char} and vwap is not null and u.ticker is not null
                 order by trans_date asc
                 """
                 )
@@ -59,6 +60,7 @@ for row in df: # name and ticker pairs
     return_rds = []
     
     def compute_return_rd():
+
         cash_random = 0
         purchase_units_random = []
         cashout_random = 0
@@ -68,9 +70,8 @@ for row in df: # name and ticker pairs
 
         trnsc_types = [trnsc[3] for trnsc in trnscs]
         for idx, trnsc in enumerate(trnscs): # all transactions of a name and ticker pair
-                # ticker = trnsc[2]
                 ps = trnsc[3] # purchase or sale
-                # date = trnsc[4]
+                date = trnsc[4]
                 amount_min = trnsc[5]
                 vwap = trnsc[6]
                 amount_max = trnsc[7]
@@ -83,33 +84,46 @@ for row in df: # name and ticker pairs
                 if "Purchase" in ps and set(["Purchase"]) == set(future_transactions): # if there's no more sales in the future, just end the loop
                     break
 
-                def _cash_out(purchase_units, cash, ps, amount, vwap, buy, cash_out, matched_buy):
+                def _cash_out(purchase_units, cash, ps, amount, vwap, buy, cash_out, matched_buy, date):
                     if "Purchase" in ps:
                         units = amount / vwap # this is the minimum amount of units purchased
-                        purchase_units.append((units, vwap))
+                        date_purchase = date.strftime('%Y-%m-01') # get the monthly rates
+                        purchase_units.append((units, vwap, date_purchase)) # fed_rate is kind of opportunity cost
                         cash -= amount
                         buy += amount
                     elif "Sale" in ps:
+                        date_sale = date.strftime('%Y-%m-01')
                         units = amount / vwap # minimum units estimated as being sold.
                         for idx, purchase in enumerate(purchase_units):
-                            stock, purchase_price = purchase
+                            stock, purchase_price, date_purchase = purchase
                             if units == 0:
                                 break                           
                             if units >= stock:
                                 units -= stock
-                                purchase_units[idx] = (0, purchase_price)
+                                purchase_units[idx] = (0, purchase_price, date_purchase)
                                 matched_buy += stock * purchase_price
-                                cash_out += stock * vwap
+
+                                purchase_date_index = fed_rates.index[fed_rates['date'] == date_purchase]
+                                sale_date_index = fed_rates.index[fed_rates['date'] == date_sale]
+                                fed_rates_avg = np.mean(fed_rates.iloc[purchase_date_index[0]:sale_date_index[0]+1]['rate'])
+                                opp_cost = fed_rates_avg * stock * purchase_price / 100
+                                cash_out += stock * vwap - opp_cost
                                 cash += stock * vwap
                             elif units < stock:
                                 units -= units
-                                purchase_units[idx] = (stock - units, purchase_price)
+                                purchase_units[idx] = (stock - units, purchase_price, date_purchase)
                                 matched_buy += units * purchase_price
-                                cash_out += units * vwap
+
+                                purchase_date_index = fed_rates.index[fed_rates['date'] == date_purchase]
+                                sale_date_index = fed_rates.index[fed_rates['date'] == date_sale]
+                                fed_rates_avg = np.mean(fed_rates.iloc[purchase_date_index[0]:sale_date_index[0]+1]['rate'])
+                                opp_cost = fed_rates_avg * units * purchase_price / 100
+
+                                cash_out += units * vwap - opp_cost
                                 cash += units * vwap                            
                     return purchase_units, cash, buy, cash_out, matched_buy
                 
-                purchase_units_random, cash_random, buy_random, cashout_random, matched_buy_random   = _cash_out(purchase_units_random, cash_random, ps, amount_random, vwap, buy_random, cashout_random, matched_buy_random)
+                purchase_units_random, cash_random, buy_random, cashout_random, matched_buy_random   = _cash_out(purchase_units_random, cash_random, ps, amount_random, vwap, buy_random, cashout_random, matched_buy_random, date)
 
         return_rd = (cashout_random - abs(matched_buy_random)) * 100 / abs(matched_buy_random) if matched_buy_random != 0 else 0
         return return_rd
@@ -141,19 +155,19 @@ for row in df: # name and ticker pairs
     ]
     pass
 
+
+    # Plot density using a kernel density estimation (KDE)
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    plt.hist(return_rds, bins=100, density=True, alpha=0.7, color='b')
+    plt.title('Density Plot of Data')
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    # plt.show()
+    plt.savefig(f'./anlys/cashout/rd-results-fed/{row}.png')
+    plt.close()
+
 import pickle
 with open(f'./anlys/cashout/result-rd.pkl', 'wb') as f:
     pickle.dump(result, f)
-
-    # # Plot density using a kernel density estimation (KDE)
-    # import numpy as np
-    # import matplotlib.pyplot as plt
-
-    # plt.hist(return_rds, bins=100, density=True, alpha=0.7, color='b')
-    # plt.title('Density Plot of Data')
-    # plt.xlabel('Value')
-    # plt.ylabel('Density')
-    # # plt.show()
-    # plt.savefig(f'./anlys/cashout/rd-results/{row}.png')
-    # plt.close()
-
