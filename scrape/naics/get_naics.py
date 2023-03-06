5d3bf0ec10204baf7c7b016d99e96f83f9266133
Suyeol Yun <syyun@mit.edu>
2023-03-06 14:32:40 -0500

    add all

--- scrape/naics/get_naics.py
+++ scrape/naics/get_naics.py
@@ -1,42 +1,40 @@
 from octopus.db import PostgresqlManager
 import psycopg2
-# pm1 = PostgresqlManager(dotenv_path="/Users/syyun/Dropbox (MIT)/efd/.env")
-# df = pm1.execute_sql(fetchall=True, sql=
-#             f"""
-#             with arr_a as (
-#                 select ticker, array_agg(distinct asset_name order by asset_name asc) as arr from senate_annual_4a
-#                 group by ticker
-#                 )
-#                 , arr_b as (
-#                 select ticker, array_agg(distinct asset_name order by asset_name asc) as arr from senate_annual_4b
-#                 group by ticker
-#                 )
-#                 , uni as (
-#                 select * from arr_a
-#                 union
-#                 select * from arr_b
-#                 )
-#                 select distinct ticker, arr[1] from uni
-#                 -- order by ticker asc
-#             """
-#                 )
 
-# # pickle df
-# import pickle
-# with open("df.pickle", "wb") as f:
-#     pickle.dump(df, f)
+from dotenv import load_dotenv
+# env = load_dotenv('/Users/syyun/Dropbox (MIT)/efd/.env')
 
-# load pickle
-import pickle
-with open("df.pickle", "rb") as f:
-    df = pickle.load(f)
+pm = PostgresqlManager(dotenv_path="/Users/syyun/Dropbox (MIT)/efd/.envlv")
+df = pm.execute_sql(fetchall=True, sql=
+            f"""
+             with arr_a as (
+                select ticker, array_agg(distinct asset_name order by asset_name asc) as arr from _sandbox_suyeol.senate_annual_4a
+                group by ticker
+                )
+                , arr_b as (
+                select ticker, array_agg(distinct asset_name order by asset_name asc) as arr from _sandbox_suyeol.senate_annual_4b
+                group by ticker
+                )
+                , uni as (
+                select * from arr_a
+                union
+                select * from arr_b
+                )
+                select  distinct uni.ticker, arr[1] as ticker_name from uni
+                	left join _sandbox_suyeol.ticker_naics_url tnu on tnu.ticker = uni.ticker
+            	where tnu.naics_url is  null
+                """
+                )
 
 if __name__ == "__main__":
-    pm2 = PostgresqlManager(dotenv_path="/Users/syyun/Dropbox (MIT)/efd/.envlv")
+    sql_insert_client_name_and_url = """
+INSERT INTO _sandbox_suyeol.ticker_naics_url (ticker, asset_name, naics_url)
+VALUES(%s, %s, %s)
+"""
 
     for ticker, asset_name in df:
+        print(ticker, asset_name)
         url = None
-        print(asset_name)
         asset_name_comps = asset_name.replace("\n", "").replace("\t", "").split(" ")
         asset_name_partial = " ".join(asset_name_comps[:2])
         query = ticker.replace("\n", "").replace("\t", "").replace(" ", "") + " " + asset_name_partial + " " + "NAICS"
@@ -48,17 +46,14 @@ if __name__ == "__main__":
         for a in a_comps:
             # check whether a has href
             if "href" not in a.attrs:
-                continue
+                continue # check next a comp
             # check whether href is a valid url
             url_cand = a.attrs["href"]
             if "https://www.naics.com/company-profile-page/" in url_cand:
                 url = url_cand
-                sql_insert_client_name_and_url = """
-                INSERT INTO _sandbox_suyeol.ticker_naics_url (ticker, asset_name, naics_url)
-                VALUES(%s, %s, %s)
-                """
+                print(url)
                 try:
-                    pm2.execute_sql(
+                    pm.execute_sql(
                         sql=sql_insert_client_name_and_url,
                         parameters=(
                             ticker,
@@ -72,8 +67,10 @@ if __name__ == "__main__":
                     pass
                 break
         if url is None:
+            url = "Not found"
+            print(url)
             try:
-                pm2.execute_sql(
+                pm.execute_sql(
                     sql=sql_insert_client_name_and_url,
                     parameters=(
                         ticker,
