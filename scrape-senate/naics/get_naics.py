3a43d168359eff0d8a6022c965322c0cf1248f6e
Suyeol Yun <syyun@mit.edu>
2023-03-28 12:11:50 -0400

    add all

--- scrape-senate/naics/get_naics.py
+++ scrape-senate/naics/get_naics.py
@@ -4,8 +4,8 @@ import psycopg2
 from dotenv import load_dotenv
 # env = load_dotenv('/Users/syyun/Dropbox (MIT)/efd/.env')
 
-pm = PostgresqlManager(dotenv_path="/Users/syyun/Dropbox (MIT)/efd/.envlv")
-# pm = PostgresqlManager(dotenv_path="/home/ubuntu/.envlv")
+# pm = PostgresqlManager(dotenv_path="/Users/syyun/Dropbox (MIT)/efd/.envlv")
+pm = PostgresqlManager(dotenv_path="/home/ubuntu/.envlv")
 
 df = pm.execute_sql(fetchall=True, sql=
             f"""
